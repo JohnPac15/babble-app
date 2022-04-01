caaf71d400fc89c6ab922ada5080f709ac0544b5
JohnPac <johnpac15@yahoo.com>
2022-04-01 00:22:01 -0500

    add server files have been added

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const {authMiddleware} = require('./utils/auth')
+
+const { typeDefs, resolvers } = require('./schema');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
