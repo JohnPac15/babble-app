668a67cb53234e6b7637e0ba34eccaa76461ea72
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-04-04 15:07:51 -0500

    styled login and signup page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -19,44 +19,40 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
   return (
-    <main >
-      <div >
-        <div >
-          <h4 >Login</h4>
-          <div >
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button  type='submit'>
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
+    <div className="form-container">
+      <div className="wrapper">
+        <h4 className="title">Login</h4>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            className="form-input"
+            placeholder="Your email"
+            name="email"
+            type="email"
+            id="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+
+          <input
+            className="form-input"
+            placeholder="******"
+            name="password"
+            type="password"
+            id="password"
+            value={formState.password}
+            onChange={handleChange}
+          />
+
+          <button type="submit">Sign in</button>
+        </form>
       </div>
-    </main>
+    </div>
   );
 };
 
