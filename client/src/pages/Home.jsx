c50bed5bbc56a815f2b646c9f7d0b0228762b8f3
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-04-06 12:28:44 -0500

    updated and styled Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import home from '../images/home.svg'
+import {Link} from 'react-router-dom'
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <div className="row">
+        <div className="col col-1">
+          <h1>Welcome to Babble!</h1>
+          <p className='bold-text'>A new and simple way to communicate!</p>
+          <p>Babble helps you connect with collegues or friends from anywhere! You can use it for business or personal use.</p>
+          <Link to='/signup'>
+          <button className='btn'>
+            Register
+          </button>
+          </Link>
+        </div>
+        <div className="col">
+          <img src={home} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
