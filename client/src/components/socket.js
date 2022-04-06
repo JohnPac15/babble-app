52e0c691cfe0f8a317ef2e71e652305649acacc0
Robert C <robert.clarady@gmail.com>
2022-04-05 19:32:48 -0500

    updated socket port

--- client/src/components/socket.js
+++ client/src/components/socket.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost");
+export const socket = io("http://localhost:3002");
 export const SocketContext = React.createContext();
