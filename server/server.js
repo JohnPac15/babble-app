2b96ed04f3e2e2cb42bd33a6e9e11e6b80236e91
John Pacini <88564390+JohnPac15@users.noreply.github.com>
2022-04-12 20:25:07 -0500

    added path to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
+const path = require('path')
 
 const { typeDefs, resolvers } = require('./schema');
 
