063c69a7c949e9ee2696264779cf8618a146fa99
Robert C <robert.clarady@gmail.com>
2022-04-13 09:32:06 -0500

    updated socket

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import io from 'socket.io-client';
 
-const socket = io("http://localhost:4000",{ autoConnect: false });
+const socket = io(`https://nameless-ocean-04231.herokuapp.com:4000`, { autoConnect: false });
 
 const useSocket = () => {
 
