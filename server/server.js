44a498bd213819d86fbe236d15582a7ee747503b
Robert C <robert.clarady@gmail.com>
2022-04-13 09:26:11 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -35,10 +35,16 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 // global server states
 const sessions = new Map();
 const globalRoomId = uuidv4();
